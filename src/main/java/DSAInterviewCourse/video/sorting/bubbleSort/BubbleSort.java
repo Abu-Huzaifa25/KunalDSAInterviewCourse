5ba9867f0ecfe7010e80bef5c7c882611206d5ed
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-03-14 23:58:24 +0530

    Updated code

--- src/main/java/DSAInterviewCourse/video/sorting/bubbleSort/BubbleSort.java
+++ src/main/java/DSAInterviewCourse/video/sorting/bubbleSort/BubbleSort.java
@@ -1,7 +1,31 @@
 package DSAInterviewCourse.video.sorting.bubbleSort;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
 
+        // Bubble sort How to work
+        int[] nums = {7, 2, 3, 17, 27, 3, 7, 49, 4, 5, 6};
+        bubbleSort(nums);
+        System.out.println(Arrays.toString(nums));
+
+
+
+    }
+
+
+    public static void bubbleSort(int [] nums){
+
+        int len = nums.length;
+        for (int i = 0; i < len - 1; i++ ){
+            for (int j = 0; j < len - i - 1; j++ ){
+
+                int temp = nums[j]; // index value of Zero ->
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
     }
 }
+
